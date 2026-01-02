2cf336659559e43a786589a7fc68f53394a6d834
Vincent Nguyen <47393759+vincendiary@users.noreply.github.com>
2026-01-02 17:48:10 -0500

    Time: 0 ms (100%), Space: 19.1 MB (18.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return [-1, -1]
+        # Solution 1 - Brute Force
+        # Time: O(n^2)
+        # Memory: O(1)
+
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # return [-1, -1]
+
+        # Solution 2 - Dictionary
+        # Time: O(n)
+        # Memory: O(n)
+
+        sumMap = {}
+        for i, n in enumerate(nums):
+            if n in sumMap:
+                return [sumMap[n], i]
+            sumMap[target - n] = i
+        return [-1, 1]
